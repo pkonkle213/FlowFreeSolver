435ee6169bebfa3f8baaa49e14b630aeb7690744
Phillip Konkle <pkonkle213@gmail.com>
2022-08-20 12:29:06 -0400

    Add project files.

---
+++ FlowFreeSolver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FlowFreeSolver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
