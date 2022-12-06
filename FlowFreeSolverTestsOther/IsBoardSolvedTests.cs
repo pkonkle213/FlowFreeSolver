b8f3d0dfed2e3868d021b2b355e234e194e8e2b3
Phillip Konkle <pkonkle213@gmail.com>
2022-12-06 14:58:50 -0500

    ready for front end start

--- FlowFreeSolverTestsOther/IsBoardSolvedTests.cs
+++ FlowFreeSolverTestsOther/IsBoardSolvedTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using FlowFreeSolver;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 
 namespace FlowFreeSolverTests
 {
@@ -9,10 +10,19 @@ namespace FlowFreeSolverTests
     {
         private PreMadeBoards _boards = new PreMadeBoards();
 
+        [TestCaseSource(nameof(SuccessfulBoards))]
         [TestMethod]
-        public void isBoardSolvesReturnsTrueForSolveableBoards()
+        public void IsBoardSolvesReturnsTrueForSolveableBoards(List<List<int>> board)
         {
 
+        }
+
+        private IEnumerable<TestCaseData> SuccessfulBoards
+        {
+            get
+            {
+                yield return new TestCaseData(_boards.boardExtreme2_30_12x12_7);
+            }
         }
     }
 }
